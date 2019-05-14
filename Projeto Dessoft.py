dd852f58233129ade85dc390b4a316a7632c7ca0
MatFreitas <matheusfs2@al.insper.edu.br>
2019-05-14 14:39:09 -0300

    Adicionei a classe CPU em documento separado

--- Projeto Dessoft.py
+++ Projeto Dessoft.py
@@ -4,6 +4,7 @@ from os import path
 
 from init import BLACK, img_dir, WIDTH, HEIGHT, FPS
 from player import Player1
+from cpu import CPU
 
 # Inicialização do Pygame.
 pygame.init()
@@ -26,50 +27,7 @@ background_rect = background.get_rect()
 
 
 
-#Classe do jogador que representa a nave
-class CPU(pygame.sprite.Sprite):
-    
-    #Construtor da classe
-    def __init__(self):
-        
-        #Construtor da classe pai (Sprite)
-        pygame.sprite.Sprite.__init__(self)
-        
-        #Carregando a imagem de fundo
-        player_img= pygame.image.load(path.join(img_dir,"CR7_v2.png")).convert()
-        self.image= player_img
-        
-        #Diminuindo o tamanho da imagem
-        self.image= pygame.transform.scale(player_img,(150,200))
-        
-        #Deixando transparente
-        self.image.set_colorkey(BLACK)
-        
-        #Detalhes sobre o posicionaento
-        self.rect= self.image.get_rect()
-        
-        #Centraliza embaixo da tela
-        self.rect.centerx= WIDTH-200
-        self.rect.bottom= HEIGHT-66
-        
-        #Velocidade
-        self.speedx=0
-        self.speedy=0
-    
-    # Metodo que atualiza a posição da navinha
-    def update(self):
-        self.rect.x += self.speedx
-        self.rect.y += self.speedy
-        
-        # Mantem dentro da tela
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.top < HEIGHT - 470:
-            self.rect.top = HEIGHT - 470
-        if self.rect.bottom > HEIGHT - 66:
-            self.rect.bottom = HEIGHT - 66
+
             
 
 player1=Player1()
