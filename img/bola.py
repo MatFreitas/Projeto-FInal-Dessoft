2cb23fa571d9b9504865fb1ea0993b6a4ca9e14c
thiagoldavid <thiagoldavid01@hotmail.com>
2019-05-14 17:48:13 -0300

    bola

---
+++ img/bola.py
@@ -0,0 +1,33 @@
+import pygame
+from os import path
+from init import BLACK, img_dir, WIDTH, HEIGHT
+
+
+class Bola(pygame.sprite.Sprite):
+    
+    #Construtor da classe
+    def __init__(self):
+        
+        #Construtor da classe pai (Sprite)
+        pygame.sprite.Sprite.__init__(self)
+        
+        #Carregando a imagem de fundo
+        player_img= pygame.image.load(path.join(img_dir,"bola.png")).convert()
+        self.image= player_img
+        
+        #Diminuindo o tamanho da imagem
+        self.image= pygame.transform.scale(player_img,(100,100))
+        
+        #Deixando transparente
+        self.image.set_colorkey(BLACK)
+        
+        #Detalhes sobre o posicionaento
+        self.rect= self.image.get_rect()
+        
+        #Centraliza embaixo da tela
+        self.rect.centerx= WIDTH-600
+        self.rect.bottom= HEIGHT-400
+        
+        #Velocidade
+        self.speedx=0
+        self.speedy=0
